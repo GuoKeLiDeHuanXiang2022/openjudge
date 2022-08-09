fea44f8a77bafb8cff7ba5fe1fe8d36e156c58d3
GuoKeLiDeHuanXiang2022 <501685891@qq.com>
2022-08-10 00:05:17 +0800

    22/08/10

---
+++ noi.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//1.1.01
+//int main()
+//{
+//	printf("Hello, World!");
+//	return 0;
+//}
+
+//1.1.02
+//int main()
+//{
+//	int a, b, c = 0;
+//	scanf("%d %d %d", &a, &b, &c);
+//	printf("%d", b);
+//	return 0;
+//}
+
+//1.1.03
+//int main()
+//{
+//	int a, b, c = 0;
+//	scanf("%d %d %d", &a, &b, &c);
+//	printf("%8d %8d %8d", a, b, c);
+//	return 0;
+//}
+
+//1.1.04
+//int main()
+//{
+//	float a = 0.0;
+//	scanf("%f", &a);
+//	printf("%.3f", a);
+//	return 0;
+//}
